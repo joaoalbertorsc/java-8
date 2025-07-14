0d1727bfe5366330cf9fa57de6e346a0c0921e1a
joaoalbertorsc <jaoalbertorsc@gmail.com>
2025-07-14 17:28:42 -0300

    Add TextBlocksTest class to demonstrate text blocks usage
    This new class includes a main method printing a multi-line string using Java text blocks and its length. It serves as a basic example for working with text blocks in Java.

---
+++ src/TextBlocksTest.java
@@ -0,0 +1,19 @@
+public class TextBlocksTest {
+    public static void main(String[] args) {
+
+        String txt = """
+                        ASDSDSDADASDASDASD
+                        ASDASDASDASD
+                        ASDSDSDDSDSDS
+                        SDSDASDASDA
+                        SDASDASD
+                        ADSDASDASDSD
+                        ASDASDASDS.....
+                        12312312312
+                        32132323232
+                        """;
+        System.out.println(txt);
+        System.out.println(txt.length());
+
+    }
+}
